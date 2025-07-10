b02838b17346e4f3f99aeedd3fe21f193cd614e1
Ng Wei Xiang <weixng97@gmail.com>
2025-07-10 22:05:26 +0800

    make new page and dashboard, change title and include bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { UsersComponent } from './pages/users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: DashboardComponent, children: [
+        { path: 'menu', component: MenuComponent },
+        { path: 'user-page', component: UsersComponent },
+    ] },
+];
